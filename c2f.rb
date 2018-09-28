dae8413ad7294120ad7d61cda43031c4e6031288
ckiesewetter360tours@gmail.com <ckiesewetter360tours@gmail.com>
2018-09-28 14:35:29 -0700

    rubytemp

---
+++ c2f.rb
@@ -0,0 +1,21 @@
+# print "Hello. Please enter a celsius value: "3
+# celsius = 100
+# fahrenheit = (celsius * 9 / 5) + 32
+# print "The fahrenheit equivalent is "
+# print fahrenheit
+# puts "."
+
+
+
+
+print "Hello. Please enter a celsius value: "
+celsius = gets
+# celsius = 100
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+print "The fahrenheit equivalent is "
+print fahrenheit
+puts "."
+#
+# puts " The Result is "
+# puts fahrenheit
+# puts "."
